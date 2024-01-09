c899c0d00850c0803bd37944ea4475e06669d787
danieldotcomcoder <daniel_shdeed@hotmail.com>
2024-01-09 16:32:17 +0200

    Refactor Display and Scientific keypad into seperate components

--- src/components/Calculator/Display.jsx
+++ src/components/Calculator/Display.jsx
@@ -1,7 +1,13 @@
+import './calculator.css';
 
 const Display = () => {
   return (
-    <div>Display</div>
+    <div className="screen">
+    <div className="lcd-container">
+      <div className="lcd-background">888888888</div>
+      <div className="lcd">42</div>
+    </div>
+  </div>
   )
 }
 
