8d476c59aaa17034382bfef01a557c3ef3971378
danieldotcomcoder <daniel_shdeed@hotmail.com>
2024-01-09 17:12:43 +0200

    Refactor ModeSelector

---
+++ src/components/Calculator/BrandContainer.jsx
@@ -0,0 +1,16 @@
+
+const BrandContainer = () => {
+  return (
+    <div className="brand-container">
+    <div className="brand">
+      <div className="text">
+        <span className="brand-text">CASIO</span>{' '}
+        <span className="model-text">fx-39</span>
+      </div>
+      <div className="sub-text">SCIENTIFIC CALCULATOR</div>
+    </div>
+  </div>
+  )
+}
+
+export default BrandContainer
--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,51 +1,24 @@
 import Display from './Display';
 import ScientificKeypad from './ScientificKeypad';
 import NormalKeypad from './NormalKeypad';
+import ModeSelector from './ModeSelector';
+import BrandContainer from './BrandContainer';
 
 const Calculator = () => {
   return (
     <div>
       <div className="casio-fx39">
-        <div className="brand-container">
-          <div className="brand">
-            <div className="text">
-              <span className="brand-text">CASIO</span>{' '}
-              <span className="model-text">fx-39</span>
-            </div>
-            <div className="sub-text">SCIENTIFIC CALCULATOR</div>
-          </div>
-        </div>
+       <BrandContainer />
 
        <Display />
 
 <ScientificKeypad />
         <NormalKeypad />
 
-      
+      <ModeSelector />
 
-        <div className="mode-selector">
-          <input id="deg" type="radio" name="mode" value="deg" />
-          <label htmlFor="deg"></label>
-
-          <input id="rad" type="radio" name="mode" value="rad" />
-          <label htmlFor="rad"></label>
-
-          <input id="gra" type="radio" name="mode" value="gra" checked />
-          <label htmlFor="gra"></label>
-
-          <input id="sd" type="radio" name="mode" value="sd" checked />
-          <label htmlFor="sd"></label>
-        </div>
-
-        <div className="mode-text">
-          <div className="option">DEG—</div>
-          <div className="option">RAD—</div>
-          <div className="option">GRA—</div>
-          <div className="option color">S D —</div>
-        </div>
-
-        <div className="casio-bottom"></div>
-      </div>
+ 
+    </div>
     </div>
   );
 };
